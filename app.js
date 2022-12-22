297cbadb3ccc2a285e94cc9007b835a5fdc8dea5
Alan Truong <alntruong@ucdavis.edu>
2022-12-22 14:51:42 -0800

    set view engine to ejs
    change sendFile to render('gride')

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
 const port = 3000
 
 app.get('/', (req, res) => {
- res.sendFile(path.join(__dirname+'/index.html'));
+ res.render('grid');
 });
 
 app.listen(port, () => {
  console.log(`App listening on port ${port}`)
 })
+
+// const teams = ['Atlanta Hawks', 'Boston Celtics', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder', 'Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards'];
+
+
+
+
+
